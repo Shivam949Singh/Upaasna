5aed39f1f7b91ddccd934f9ac0ef64e469e83115
Shivam Singh <shivam949singh@gmail.com>
2024-06-30 19:16:09 +0530

    seprated the title and description and added js

--- js/god.js
+++ js/god.js
@@ -19,30 +19,58 @@ document.addEventListener('DOMContentLoaded', function() {
       })
       .then(data => {
           console.log('Received data:', data);
-          const aartiSection = document.getElementById('aarti');
-          const chalisaSection = document.getElementById('chalisa');
-          const stotraSection = document.getElementById('stotra');
-          console.log('aartiSection : ', aartiSection);
-          if (aartiSection) {
-                  const div = document.createElement('div');
-                  div.innerHTML = `<h1>${data[0].title}</h1><h2>${data[0].description}</h2>`;
-                  aartiSection.appendChild(div);
+          const aartiTitle = document.getElementById('aarti_title');
+          const aartiDescription = document.getElementById('aarti_description');
+          const chalisaTitle = document.getElementById('chalisa_title');
+          const chalisaDescription = document.getElementById('chalisa_description');
+          const stotraTitle = document.getElementById('stotra_title');
+          const stotraDescription = document.getElementById('stotra_description');
+          if (aartiTitle) {
+            const div = document.createElement('div');
+            div.innerHTML = `<h1>${data[0].title}</h1>`;
+            aartiTitle.appendChild(div);
           } else {
-              console.error('Aarti section not found in the DOM');
+              console.error('Aarti Title not found in the DOM');
           }
-          if (chalisaSection) {
-                  const div = document.createElement('div');
-                  div.innerHTML = `<h1>${data[1].title}</h1><h2>${data[1].description}</h2>`;
-                  chalisaSection.appendChild(div);
+
+          if (aartiDescription) {
+            const div = document.createElement('div');
+            div.innerHTML = `<h2>${data[0].description}</h2>`;
+            aartiDescription.appendChild(div);
           } else {
-              console.error('Aarti section not found in the DOM');
+              console.error('Aarti Description not found in the DOM');
           }
-          if (stotraSection) {
+
+          if (chalisaTitle) {
                   const div = document.createElement('div');
-                  div.innerHTML = `<h1>${data[2].title}</h1><h2>${data[2].description}</h2>`;
-                  stotraSection.appendChild(div);
+                  div.innerHTML = `<h1>${data[1].title}</h1>`;//<h2>${data[1].description}</h2>`;
+                  chalisaTitle.appendChild(div);
           } else {
-              console.error('Aarti section not found in the DOM');
+              console.error('Chalisa Title not found in the DOM');
+          }
+
+          if (chalisaDescription) {
+            const div = document.createElement('div');
+            div.innerHTML = `<h2>${data[1].description}</h2>`;
+            chalisaDescription.appendChild(div);
+          } else {
+              console.error('Aarti Description not found in the DOM');
+          }
+
+          if (stotraTitle) {
+                  const div = document.createElement('div');
+                  div.innerHTML = `<h1>${data[2].title}</h1>`;//<h2>${data[2].description}</h2>`;
+                  stotraTitle.appendChild(div);
+          } else {
+              console.error('Strotra title not found in the DOM');
+          }
+
+          if (stotraDescription) {
+            const div = document.createElement('div');
+            div.innerHTML = `<h2>${data[2].description}</h2>`;
+            stotraDescription.appendChild(div);
+          } else {
+              console.error('Aarti Description not found in the DOM');
           }
       })
       .catch(error => console.error('Error fetching Aarti data:', error)
